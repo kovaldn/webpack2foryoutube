65ec7bf06756359b6a4a281e20cc1efaf22a7edd
Dmitriy <kovaldn@gmail.com>
2017-04-08 00:08:06 +0300

    pug and pug-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,18 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack app'
+            template: PATHS.source + '/index.pug'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
+        ]
+    }
 };
