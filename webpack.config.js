dba77c811d6f215aa4e3b202a5600f7120c488cc
Dmitriy <kovaldn@gmail.com>
2017-04-28 16:48:01 +0300

    svg add

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const sass = require('./webpack/sass');
 const css = require('./webpack/css');
 const extractCSS = require('./webpack/css.extract');
 const uglifyJS = require('./webpack/js.uglify');
+const images = require('./webpack/images');
 
 const PATHS = {
     source: path.join(__dirname, 'source'),
@@ -44,7 +45,8 @@ const common = merge([
             })
         ]
     },
-    pug()
+    pug(),
+    images()
 ]);
 
 module.exports = function(env) {
